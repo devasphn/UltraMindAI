b51bfcd6f7673e9bd7177bf05c62ba6fa17e151b
Deva Kumar (AIML) <117890712+devasphn@users.noreply.github.com>
2025-07-09 16:26:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-# config.py
+# config.py (Corrected Version)
 import torch
+import os
 
 # --- General Settings ---
 SERVER_HOST = "0.0.0.0"
@@ -8,12 +9,12 @@ LOG_LEVEL = "INFO"
 
 # --- WebRTC & Audio Settings ---
 STUN_SERVER = "stun:stun.l.google.com:19302"
-AUDIO_SAMPLE_RATE_IN = 48000  # Input from WebRTC
-AUDIO_SAMPLE_RATE_VAD = 16000 # Required by Silero VAD
-AUDIO_SAMPLE_RATE_ASR = 16000 # Required by Whisper
-AUDIO_SAMPLE_RATE_EMO = 16000 # Required by Emotion Model
-AUDIO_SAMPLE_RATE_TTS = 24000 # Output from StyleTTS2
-AUDIO_SAMPLE_RATE_OUT = 48000 # Required by WebRTC
+AUDIO_SAMPLE_RATE_IN = 48000
+AUDIO_SAMPLE_RATE_VAD = 16000
+AUDIO_SAMPLE_RATE_ASR = 16000
+AUDIO_SAMPLE_RATE_EMO = 16000
+AUDIO_SAMPLE_RATE_TTS = 24000
+AUDIO_SAMPLE_RATE_OUT = 48000
 
 # --- VAD Settings ---
 VAD_MIN_SPEECH_MS = 250
@@ -34,9 +35,11 @@ EMOTION_MODEL = "audeering/wav2vec2-large-robust-12-ft-emotion-msp-dim"
 # LLM (Llama 3)
 LLM_MODEL = "meta-llama/Meta-Llama-3-8B-Instruct"
 
-# TTS (StyleTTS 2)
-TTS_MODEL = "yl4579/StyleTTS2-LibriTTS"
-TTS_CONFIG = "yl4579/StyleTTS2-LibriTTS/config.yml"
+# TTS (StyleTTS 2) - Paths are now local
+# We will create these directories and files in the setup commands
+TTS_MODEL_DIR = os.path.join("StyleTTS2", "Models", "LibriTTS")
+TTS_MODEL_PATH = os.path.join(TTS_MODEL_DIR, "epochs_2nd_00020.pth")
+TTS_CONFIG_PATH = os.path.join(TTS_MODEL_DIR, "config.yml")
 
 # --- Memory Settings ---
 MEMORY_DB_PATH = "./chroma_db"
